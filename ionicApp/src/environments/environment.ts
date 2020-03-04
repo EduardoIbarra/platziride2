646765cfa4ed0ddda126d545c99158cc71e809c1
Eduardo <hugo.delacruz@neoris.com>
2020-03-03 20:14:49 -0600

    Removed keys

--- ionicApp/src/environments/environment.ts
+++ ionicApp/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   api_url: 'http://localhost:1337/',
   openWeatherMap_url: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?',
-  openWeatherMap_key: '985b733b5749f621ad8ca0d0fa9f6dcc',
+  openWeatherMap_key: 'REEMPLAZA_CON_TU_LLAVE',
 };
 
 /*
